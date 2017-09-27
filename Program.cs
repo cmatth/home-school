f949689b087b37434f98dca97523a43b5b9bb47f
casey <cmatthew@uccs.edu>
2017-09-26 22:14:50 -0600

    program2 completed

--- Program.cs
+++ Program.cs
@@ -8,102 +8,104 @@ namespace program1
         static void Main()
         {
             string[] input  = System.IO.File.ReadAllLines(@"MachineInstructions.txt");
-            string[] output = disassembleMIPS(input);
-            System.IO.File.WriteAllLines(@"Program1.txt", output);
+            executeMIPS(input);
         }
 
-        static public string[] disassembleMIPS(string[] program)
+        static public void executeMIPS(string[] program)
         {
-            // Only Supports instructions present in example.
-            string[] opCodes = new string[44];
-            opCodes[0]  = "syscall";
-            opCodes[2]  = "j";
-            opCodes[4]  = "beq";
-            opCodes[8]  = "addi";
-            opCodes[13] = "ori";
-            opCodes[15] = "lui";
-            opCodes[35] = "lw";
-            opCodes[43] = "sw";
-            
-            string[] registers = { "$0", "$at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3", "$t0", "$t1", "$t2", "$t3" };
-            string[] decoded = new string[program.Length];
-        
-            for(int i = 0; i < program.Length; i++)
+            uint PC = Convert.ToUInt32(program[0].Split('\t')[0],16);
+            int[] registers = new int[12];
+            bool halt = false; //syscall sets to true
+            int cycleCount = 0;
+
+            MemoryModel IM = new MemoryModel(PC, program.Length);
+            for(uint i = 0; i < program.Length; i++)
             {
-                string[]   input = program[i].Split('\t');
-                uint addr = Convert.ToUInt32(input[0],16);
-                uint inst = Convert.ToUInt32(input[1],16);
+                IM.Store(PC + (i * 4), Convert.ToUInt32(program[i].Split('\t')[1],16));
+            }
+
+            MemoryModel DM = new MemoryModel(0x10010000u, 9);
+            for(uint i = 0; i < 9; i++)
+            {
+                DM.Store(0x10010000u + (i * 4), i);
+            }
+            DM.Dump(@"DMDumpBefore.txt");
+
+            while(!halt)
+            {
+                cycleCount++;
+                uint inst = IM.Load(PC);
+                PC += 4;
 
                 uint funCode = inst >> 26;
                 uint rs = (inst >> 21) & 0x1F;
                 uint rt = (inst >> 16) & 0x1F;     
-                int imm = (short)(inst & 0xFFFF);   
+                int imm = (short)inst & 0xFFFF;   
                 uint jAddr = (inst & 0x03FFFFFF) << 2;
+                uint func = inst & 0x1F;
 
                 switch(funCode)
                 {
-                    case 0: //syscall
-                        decoded[i] = String.Format("0x{0:x08}\t{1}", addr, opCodes[0]);
+                    case 0: //syscall   
+                        if(func == 12) halt = true;
                         break;
                     case 2: //j
-                        decoded[i] = String.Format("0x{0:x08}\t{1}      0x{2:x08}", addr, opCodes[2], jAddr);
+                        PC = jAddr;
                         break;
                     case 4://beq
-                        decoded[i] = String.Format("0x{0:x08}\t{1}    {2}, {3}, {4}", addr, opCodes[4], registers[rt], registers[rs], imm);
+                        if(registers[rs] == registers[rt]) PC += Convert.ToUInt32(imm << 2);
                         break;
                     case 8://addi
-                        decoded[i] = String.Format("0x{0:x08}\t{1}   {2}, {3}, {4}", addr, opCodes[8], registers[rt], registers[rs], imm);
+                        registers[rt] = registers[rs] + imm;
                         break;
                     case 13://ori
-                        decoded[i] = String.Format("0x{0:x08}\t{1}    {2}, {3}, {4}", addr, opCodes[13], registers[rt], registers[rs], imm);
+                        registers[rt] = registers[rs] ^ imm;
                         break;
                     case 15://lui
-                        decoded[i] = String.Format("0x{0:x08}\t{1}    {2}, {3}", addr, opCodes[15], registers[rt], imm);
+                        registers[rt] = imm << 16;
                         break;
                     case 35://lw
-                        decoded[i] = String.Format("0x{0:x08}\t{1}     {2}, {3}({4})", addr, opCodes[35], registers[rt], imm, registers[rs]);
+                        registers[rt] = Convert.ToInt32(DM.Load(Convert.ToUInt32(registers[rs] + imm)));
                         break;
                     case 43://sw
-                        decoded[i] = String.Format("0x{0:x08}\t{1}     {2}, {3}({4})", addr, opCodes[43], registers[rt], imm, registers[rs]);
+                        DM.Store(Convert.ToUInt32(registers[rs] + imm), Convert.ToUInt32(registers[rt]));
                         break;
                     default:
-                        decoded[i] = "Instruction not supported.";
                         break;
                 }
             }
-            return decoded;
+            IM.Dump(@"IMDump.txt");
+            DM.Dump(@"DMDumpAfter.txt");
         }
     }
 
     class MemoryModel
     {
         private uint BaseAddress;
-        private int[] MemorySpace;
+        private uint[] MemorySpace;
 
         public MemoryModel(uint _baseAddress, int _size)
         {
             BaseAddress = _baseAddress;
-            MemorySpace = new int[_size];
+            MemorySpace = new uint[_size];
         }
-
-        public int Load(uint address)
+        public uint Load(uint address)
         {
             return MemorySpace[(address - BaseAddress) >> 2];
         }
-
-        public void Store(uint address, int data)
+        public void Store(uint address, uint data)
         {
             MemorySpace[(address - BaseAddress) >> 2] = data;
         }
 
-        public void Dump()
+        public void Dump(string filename)
         {
             string[] memoryData = new string[MemorySpace.Length];
             for(int i=0; i < MemorySpace.Length; i++)
             {
-                memoryData[i] = String.Format("0x{0:08x}\t0x{1:08x}", (i << 2) + BaseAddress, MemorySpace[i]);
+                memoryData[i] = String.Format("0x{0:x08}\t0x{1:x08}", (i << 2) + BaseAddress, MemorySpace[i]);
             }
-            System.IO.File.WriteAllLines(@"MemoryDump.txt", memoryData);
+            System.IO.File.WriteAllLines(@filename, memoryData);
         }
     }
 }
